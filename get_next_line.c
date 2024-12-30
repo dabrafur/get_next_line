e0afce58438b07ef45ecc7532e3db9387ca93a0a
debsfurlan <103475619+debsfurlan@users.noreply.github.com>
2024-12-30 20:17:34 -0300

    some adds on get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,27 +1,86 @@
 #include "get_next_line.h"
 
-static char *read_line(int fd, char *buf)
+static char	*free_buf(char *buf, char *read_buf)
 {
-
+	free(read_buf);
+	free(buf);
+	return (NULL);
 }
 
-
-char *get_next_line(int fd)
+static char	*read_line(int fd, char	*buf)
 {
-    static char *buf;
-    char *line; //onde vou guardar o que foi lido com read
+	char	*read_buf;
+	char	*temp_line;
+	int		bytes_read;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    
-    buf = read_line(fd, buf);
-    line = get_line(buf);
+	bytes_read = 1;
+	read_buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!read_buf)
+		return (NULL);
+	while (bytes_read != 0)
+	{
+		bytes_read = read(fd, read_buf, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (free_buff(buf, read_buf));
+		read_buf[bytes_read] = '\0';
+		temp_line = buf;
+		buf = ft_strjoin(temp_line, read_buf);
+		free(temp_line);
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	free(read_buf);
+	return (buf);
+}
 
-    
+static char	*cut_line(char *buf)
+{
+	int		i;
+	char	*new_line;
 
+	i = 0;
+	if (!buf[i])
+		return (NULL);
+	while (buf[i] != '\n' && buf[i] != '\0')
+		i++;
+	new_line = ft_substr(buf, 0, i + 1);
+	return (new_line);
+}
 
+static char	*new_line(char *buf)
+{
+	int		i;
+	char	*temp;
 
+	i = 0;
+	while (buf[i] != '\n' && buf[i] != '\0')
+		i++;
+	if (!buf[i])
+	{
+		free(buf);
+		return (NULL);
+	}
+	temp = ft_substr(buf, i + 1, ft_strlen(buf) - 1);
+	free(buf);
+	return (temp);
+}
 
+char	*get_next_line(int fd)
+{
+	static char	*buf;
+	char		*line;
 
-    return (line);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!buf)
+		buf = ft_strdup("");
+	buf = read_line(fd, buf);
+	if (!buf)
+	{
+		free(buf);
+		return (NULL);
+	}
+	line = cut_line(buf);
+	buf = new_line(buf);
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,5 +1,4 @@
 #include "get_next_line.h"
-//#include <stdio.h>
 
 size_t ft_strlen(const char *str) //retorna comprimento de str
 {
