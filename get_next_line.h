4740086dc851067c17508d4c2910e6ca73c9263f
Debora Abrahao Furlan <deabraha@c1r3p1.42sp.org.br>
2024-12-31 09:57:09 -0300

    norminette + tests

--- get_next_line.h
+++ get_next_line.h
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deabraha <deabraha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 09:39:08 by deabraha          #+#    #+#             */
+/*   Updated: 2024/12/31 09:39:47 by deabraha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
@@ -14,7 +26,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t  ft_strlen(const char *str);
-char    *get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*get_next_line(int fd);
 
 #endif
--- main.c
+++ main.c
@@ -1,22 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deabraha <deabraha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 09:52:18 by deabraha          #+#    #+#             */
+/*   Updated: 2024/12/31 09:54:09 by deabraha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int main (void)
+int	main(void)
 {
-    int fd = open("text.txt", O_RDONLY);;
-    char *line;
-    
-    if (fd == -1)
-    {
-        perror("Error opening file");
-        return 1;
-    }
-    
-    while ((line = get_next_line(fd)) != NULL )
-    {
-        printf("%s", line);
-        free(line);
-    }
-    close(fd);
-    
-    return (0);
+	char	*line;
+	int		fd;
+
+	fd = open("text.txt". O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+	while ((line = get_next_line(fd)) != NULL )
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
 }
